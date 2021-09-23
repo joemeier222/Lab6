745a582efaeb6af84eb6a4f9b27b654f514ad802
Joseph Meier <joseph.meier@go.tarleton.edu>
2021-09-23 16:05:28 -0600

    Question updated

--- src/main/java/tarleton/lab6/Question.java
+++ src/main/java/tarleton/lab6/Question.java
@@ -5,5 +5,10 @@ package tarleton.lab6;
  * @author Joseph Meier
  */
 public class Question {
-    
+    private int id;
+
+    @Override
+    public String toString() {
+        return Integer.toString(id);
+    }
 }
