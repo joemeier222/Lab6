07b4672f9725568609a387da9d0a73ac8cd2a770
Joseph Meier <joseph.meier@go.tarleton.edu>
2021-09-23 15:59:52 -0600

    Initial Commit

---
+++ src/main/java/tarleton/lab6/Main.java
@@ -0,0 +1,11 @@
+package tarleton.lab6;
+
+/**
+ *
+ * @author Joseph Meier
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/tarleton/lab6/Question.java
@@ -0,0 +1,9 @@
+package tarleton.lab6;
+
+/**
+ *
+ * @author Joseph Meier
+ */
+public class Question {
+    
+}
