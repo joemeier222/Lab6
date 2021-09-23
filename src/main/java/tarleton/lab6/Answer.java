35e4d976f26c150408ec5939d01b4eb1218a1e71
Joe Meier <joemeier222@gmail.com>
2021-09-23 16:09:56 -0500

    Answer added, Question updated

---
+++ src/main/java/tarleton/lab6/Answer.java
@@ -0,0 +1,9 @@
+package tarleton.lab6;
+
+/**
+ *
+ * @author Joe Meier
+ */
+public class Answer {
+
+}
--- src/main/java/tarleton/lab6/Question.java
+++ src/main/java/tarleton/lab6/Question.java
@@ -5,5 +5,11 @@ package tarleton.lab6;
  * @author Joseph Meier
  */
 public class Question {
-    
+    private String text;
+
+    @Override
+    public String toString() {
+        return text;
+    }
+   
 }
