4874e98e4f9d83be31b1f9c3df00735e8e40a075
Joe Meier <joemeier222@gmail.com>
2021-09-23 17:10:16 -0500

    User B: Update 16

--- src/main/java/tarleton/lab6/Answer.java
+++ src/main/java/tarleton/lab6/Answer.java
@@ -5,5 +5,10 @@ package tarleton.lab6;
  * @author Joe Meier
  */
 public class Answer {
+    private String text;
 
+    @Override
+    public String toString() {
+        return text;
+    }
 }
--- src/main/java/tarleton/lab6/Main.java
+++ src/main/java/tarleton/lab6/Main.java
@@ -1,11 +1,32 @@
 package tarleton.lab6;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Joseph Meier
  */
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            Path pc = Paths.get("input.txt");
+            Charset cs = Charset.forName("UTF-8");
+            List<String> lines = Files.readAllLines(pc, cs);
+            List<Question> questions = new ArrayList<>();
+            for (String line : lines){
+                String[] s = line.split("\\s+");
+                Question q = new Question(Integer.parseInt(s[0]), s[1]);
+                questions.add(q);
+            }
+            System.out.println(questions.get(0).toString());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
